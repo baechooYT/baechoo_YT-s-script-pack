e97dfe6a83469289ba90f58e2aef0c9815005ad2
beachoo_Git <66154521+baechooYT@users.noreply.github.com>
2021-01-03 13:46:26 +0900

    Update acs kill all.lua

--- Scripts/Fe scripts/acs kill all.lua
+++ Scripts/Fe scripts/acs kill all.lua
@@ -1,7 +1,7 @@
-while wait(0.01) do
-    local DR = game:GetService("ReplicatedStorage")["ACS_Engine"].Eventos.Damage
-for PlayerIndex,GivenPlayer in pairs(game.Players:GetPlayers()) do
-    local Char = GivenPlayer.Character
-    DR:FireServer(Char.Humanoid,Char.Humanoid.MaxHealth,0,0)
+while wait(0.01) do --loop
+    local DR = game:GetService("ReplicatedStorage")["ACS_Engine"].Eventos.Damage --get service damage
+for PlayerIndex,GivenPlayer in pairs(game.Players:GetPlayers()) do -- find players
+    local Char = GivenPlayer.Character -- find character
+    DR:FireServer(Char.Humanoid,Char.Humanoid.MaxHealth,0,0) -- make humanoid maxhealth 0
 end
 end
